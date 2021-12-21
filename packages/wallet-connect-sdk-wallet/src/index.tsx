47951ce78abebbebc2b7c400a50d0822f928b4fe
Gil Lopes Bueno <melanke.hc@gmail.com>
2021-12-21 12:47:06 -0300

    Fix setSession to avoid duplicates

--- packages/wallet-connect-sdk-wallet/src/index.tsx
+++ packages/wallet-connect-sdk-wallet/src/index.tsx
@@ -358,7 +358,14 @@ export const WalletConnectContextProvider: React.FC<{
       console.log('EVENT', 'session_deleted')
       setSessions(wcClient.session.values)
     })
-  }, [chains, checkApprovedRequest, makeRequest, respondRequest, wcClient, JSON.stringify(sessions)])
+  }, [
+    chains,
+    checkApprovedRequest,
+    makeRequest,
+    respondRequest,
+    wcClient,
+    JSON.stringify(sessions),
+  ])
 
   const findSessionByTopic = useCallback(
     (topic: string) => {
@@ -415,7 +422,21 @@ export const WalletConnectContextProvider: React.FC<{
     }
     const session = await wcClient.approve({proposal, response})
     setSessionProposals((old) => old.filter((i) => i !== proposal))
-    setSessions([session])
+    setSessions((old) => {
+      if (
+        old.find(
+          (oldSession) =>
+            oldSession.topic === session.topic &&
+            oldSession.state.accounts.some((account) =>
+              session.state.accounts.includes(account)
+            )
+        )
+      ) {
+        return old
+      } else {
+        return [...old, session]
+      }
+    })
   }
 
   const rejectSession = async (proposal: SessionTypes.Proposal) => {
